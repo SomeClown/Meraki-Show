9d6e062e279f6cdd19cc1cde1a9e134eb433419f
SomeClown <justsomeclown@gmail.com>
2016-08-25 17:59:42 -0700

    Added and/or changed the following: (1) refactored into a more modularized code (definitions, separate main function, etc. (2) Added argparse funtionality to further expand usefulness in the future (3) Changed main function to its own function (4) Added code to check if loaded as main, or as a module via import in another program
    Still need to add better error checking. On one run through, the Meraki
    server returned a "buffering..." string, which blew up the program. Need
    to add code to catch expected error codes, as well as unexpected
    codes/problems
    
    Also need to change kludgey code to be more pep-8 compliant

--- showmeraki3.py
+++ showmeraki3.py
@@ -1,23 +1,78 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python3
 
 #------------------------------------------------------------------------------------
 	# apikeys is a .py file that is in the same directory as this script. setup is meraki="your api key"
-import requests
-import apikeys
+import requests, sys, os, argparse, apikeys
 from pprint import pprint
+
+progVersion = str('alpha 0.1')
+
+#TODO: refactor to make this less kludgey and more Pep 8 compliant
+#TODO: (https://www.python.org/dev/peps/pep-0008/)
+
+global key
+global url
+global headers
 
 key = apikeys.meraki
 url = "https://n49.meraki.com/api/v0/"
-
-main = input("What main function API are you calling? ")
-net = input("For what organization or network? " )
-info = input("What information are you looking for? ")
-
 headers = {
     'X-Cisco-Meraki-API-Key': key,
     'Content-Type': 'application/json',
-}
+    }
 
-r=requests.get(url + main + net + info, headers=headers)
-r.json()
-pprint (r.json())
+def printRequest(_url, main, net, info):
+	r=requests.get(_url + main + net + info, headers=headers)
+	r.json()
+	pprint (r.json())
+
+def saveRequest(_url, main, net, info):
+	r=requests.get(_url + main + net + info, headers=headers)
+	with open('stuffFile', 'w') as ourFile:
+		ourFile.write(mainThings)
+		ourFile.write(netThings)
+		ourFile.write(infoThings)
+
+def getStuff():
+	stuffs = []
+	main = input("What main function API are you calling? ")
+	net = input("For what organization or network? " )
+	info = input("What information are you looking for? ")
+	stuffs.append(main)
+	stuffs.append(net)
+	stuffs.append(info)
+	return(stuffs)
+
+def main():
+	parser = argparse.ArgumentParser(description='Meraki (enterprise grade) grabber of stuff', epilog='For questions, talk to @wifijanitor')
+	parser.add_argument('-d', '--display', action='store_true', dest='displayStuff', help='Display results to screen')
+	parser.add_argument('-s', '--save', action='store_true', dest='saveStuff', help='Save results to file')
+	commands = parser.parse_args()
+	if commands.displayStuff:
+		try:
+			stuffs = getStuff()
+			print(stuffs)
+			mainThings = stuffs[0]
+			netThings = stuffs[1]
+			infoThings = stuffs[2]
+			printRequest(url, mainThings, netThings, infoThings)
+		except (SystemExit):
+			raise
+		except (KeyboardInterrupt):
+			logging.exception
+	elif commands.saveStuff:
+		try:
+			stuffs = getStuff()
+			mainThings = stuffs[0]
+			netThings = stuffs[1]
+			infoThings = stuffs[2]
+			saveRequest(url, mainThings, netThings, infoThings)
+		except (SystemExit):
+			raise
+		except (KeyboardInterrupt):
+			logging.exception
+
+if __name__ == '__main__':
+	main()
+
+else: print("loaded as module, api, or Skynet...")
